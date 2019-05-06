e0738361fe052501febf15c1833f5e1fc766365e
xinzheshen <xinzhe.shen@cienet.com.cn>
2019-05-06 10:26:29 +0800

    add detect_sound_mix

--- test/audioprocess/detect_sound_pause_or_mix.py
+++ test/audioprocess/detect_sound_pause_or_mix.py
@@ -1,11 +1,15 @@
-# -*- coding: utf8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import numpy as np
-from scipy.signal import stft
-import copy
+import scipy.io.wavfile
 from collections import OrderedDict
+import sys
+import time
+import argparse
+
 
 def calc_stft(signal, sample_rate=16000, frame_size=0.025, frame_stride=0.01, winfunc=np.hamming, NFFT=512):
-
     # Calculate the number of frames from the signal
     frame_length = frame_size * sample_rate
     frame_step = frame_stride * sample_rate
@@ -35,52 +39,55 @@ def calc_stft(signal, sample_rate=16000, frame_size=0.025, frame_stride=0.01, wi
 
     return pow_frames
 
+
 def detect_mix(pow_spec):
-    start_time = 0
-    last_valid_frame = 0
     result = OrderedDict()
+
+    start_time = 0
+    last_valid_freq = 0
+    last_valid_freq_power = 0
     number = 0
+    first_time = True
     for index_time, powers in enumerate(pow_spec):
-        max_power_index = np.argmax(powers)
-        max_power = powers[max_power_index]
+        index_time = index_time/100.0
+        max_power_freq_index = np.argmax(powers)
+        max_power = powers[max_power_freq_index]
         # Normally the power should be bigger than 5000 when there is voice
         if max_power > 5000:
-            if abs(max_power_index - last_valid_frame) < 5:
+            valid_frequencies = np.where(powers > max_power * 0.9)
+            max_freq = max(valid_frequencies[0])
+            min_freq = min(valid_frequencies[0])
+
+            # The audio is single freq, but the got data may not be single.
+            if abs(max_power_freq_index - last_valid_freq) < 50:
                 duration = str(start_time) + '-' + str(index_time)
             else:
                 start_time = index_time
                 number += 1
                 duration = str(start_time) + '-' + str(index_time)
+                if first_time:
+                    first_time = False
+                elif powers[last_valid_freq] >= last_valid_freq_power:
+                    sys.stderr.write('The time of mixing sound: ' + str(index_time))
+                    exit(100)
             if number == 0:
                 number += 1
             result[number] = duration
-            last_valid_frame = max_power_index
+            last_valid_freq = max_power_freq_index
+            last_valid_freq_power = max_power
+    return result
 
 
-if __name__ == '__main__':
-    import scipy.io.wavfile
-
-    # Read wav file
-    # "OSR_us_000_0010_8k.wav" is downloaded from http://www.voiptroubleshooter.com/open_speech/american.html
-    sample_rate, signal = scipy.io.wavfile.read(r"D:\cats\bugs\1812\8000+music+8000.wav")
-    # sample_rate, signal = scipy.io.wavfile.read(r"D:\cats\bugs\1812\400-800-1200-1600.wav")
-    # Get speech data in the first 2 seconds
-    # signal = signal[0:int(2. * sample_rate)]
-    # signal = signal[int(0.4 * sample_rate):int(1. * sample_rate)]
-
-    # Calculate the short time fourier transform
-    pow_spec = calc_stft(signal, sample_rate, NFFT=sample_rate)
+def detect_pause(pow_spec):
+    result = OrderedDict()
 
     start_time = 0
-    last_end_time = 0
     last_valid_freq = 0
     last_valid_freq_power = 0
-    result = OrderedDict()
     number = 0
     is_multi_freq = False
     first_time = True
     new_section = True
-    valid_frequencies = []
     max_valid_freq = 0
     for index_time, powers in enumerate(pow_spec):
         index_time = index_time/100.0
@@ -89,7 +96,11 @@ if __name__ == '__main__':
         max_valid_freq = max_valid_freq if (max_valid_freq > max_power_freq_index) else max_power_freq_index
         # Normally the power should be bigger than 5000 when there is voice
         if max_power > 5000:
-            if abs(max_power_freq_index - last_valid_freq) < 5:
+            valid_frequencies = np.where(powers > max_power * 0.05)
+            max_freq = max(valid_frequencies[0])
+            min_freq = min(valid_frequencies[0])
+
+            if abs(max_power_freq_index - last_valid_freq) < 50:
                 if powers[max_valid_freq] < max_power * 0.7:
                     # this means it's in multi_freq_section
                     pass
@@ -110,11 +121,13 @@ if __name__ == '__main__':
                         is_multi_freq = True
                         new_section = True
                     if new_section:
+                        tmp = powers[last_valid_freq]
                         number += 1
                         start_time = index_time
                         new_section = False
-                        if powers[last_valid_freq] > last_valid_freq_power:
-                            print('--------------error----------: ' + str(last_valid_freq))
+                        if powers[last_valid_freq] >= last_valid_freq_power or (max_freq - min_freq) > 5000:
+                            sys.stderr.write('The time of mixing sound: ' + str(index_time))
+                            exit(100)
 
             duration = str(start_time) + '-' + str(index_time)
 
@@ -122,8 +135,25 @@ if __name__ == '__main__':
                 result[number] = duration
             last_valid_freq = max_power_freq_index
             last_valid_freq_power = max_power
+    return result
 
 
+def process_argv():
+    parser = argparse.ArgumentParser(prog='silence_detect')
+    parser.add_argument('--file', '-f', help='The path of the audio file.', required=True)
+    parser.add_argument('--allSingleFreq', '-a', help='If the audio is all single frequency.', choices=['true', 'false'], default='true')
+    return parser.parse_args()
 
-    # f, t, Zxx = stft(signal, fs=sample_rate, nperseg=512, nfft=sample_rate)
-    print("over")
+
+if __name__ == '__main__':
+    arg = process_argv()
+    # sample_rate, signal = scipy.io.wavfile.read(r"D:\cats\bugs\1812\400-800-1200-1600.wav")
+    sample_rate, signal = scipy.io.wavfile.read(arg.file)
+    pow_spec = calc_stft(signal, sample_rate, NFFT=sample_rate)
+    if arg.allSingleFreq == 'true':
+        result = detect_mix(pow_spec)
+    else:
+        result = detect_pause(pow_spec)
+    print ('The index and start-end time:')
+    for key, value in result.items():
+        print (str(key) + ' : ' + value)
